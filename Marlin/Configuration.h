e1a93e34f1edae357c8b86fccfc44b49e79b7d4d
moophlo <andrea.odorisio@gmail.com>
2020-10-25 19:19:25 +0100

    runout filament mm70 - enable junction deviation

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -785,7 +785,7 @@
  * When changing speed and direction, if the difference is less than the
  * value set here, it may happen instantaneously.
  */
-#define CLASSIC_JERK
+//#define CLASSIC_JERK
 #if ENABLED(CLASSIC_JERK)
   #define DEFAULT_XJERK 8.0
   #define DEFAULT_YJERK 8.0
@@ -1181,7 +1181,7 @@
   // After a runout is detected, continue printing this length of filament
   // before executing the runout script. Useful for a sensor at the end of
   // a feed tube. Requires 4 bytes SRAM per sensor, plus 4 bytes overhead.
-  #define FILAMENT_RUNOUT_DISTANCE_MM 50
+  #define FILAMENT_RUNOUT_DISTANCE_MM 70
 
   #ifdef FILAMENT_RUNOUT_DISTANCE_MM
     // Enable this option to use an encoder disc that toggles the runout pin
